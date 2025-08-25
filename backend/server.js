0e7e9f57c7b7c4bdd41532474a9e5638b9e43163
Rasya Rahmat Syaban <rasyarahmatsyban10@gmail.com>
2025-08-25 22:39:49 +0800

    feat(docker): add Dockerfile and fix db password variable name

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,50 @@
-import express from "express"
-import cors from "cors"
-import router from "./src/routes/index.js"
-import "dotenv/config"
-import path from "path"
+import express from "express";
+import cors from "cors";
+import router from "./src/routes/index.js";
+import "dotenv/config";
+import path from "path";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const allowedOrigins = [
-    "http://localhost:5173", // Vite dev server
-    "http://192.168.10.202:5173", //contoh url lan
-    process.env.FRONTEND_URL,
-]
+  "http://localhost:5173", // Vite dev server
+  "http://192.168.10.202:5173", //contoh url lan
+  process.env.FRONTEND_URL,
+];
 
-
+console.log("ini env", process.env.FRONTEND_URL);
 // Update CORS configuration to include your ngrok URLs
 app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"],
-    }),
-)
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "ngrok-skip-browser-warning",
+    ],
+  })
+);
 
 // Add middleware to handle ngrok browser warning
 app.use((req, res, next) => {
-    res.header("ngrok-skip-browser-warning", "true")
-    next()
-})
+  res.header("ngrok-skip-browser-warning", "true");
+  next();
+});
 
-app.use(express.json())
-app.use("/", router)
+app.use(express.json());
+app.use("/", router);
 
-app.listen(port, () =>
-    console.log(`Server berjalan di port ${port}`),
-    console.log("Allowed origins:", allowedOrigins)
-)
+app.listen(
+  port,
+  () => console.log(`Server berjalan di port ${port}`),
+  console.log("Allowed origins:", allowedOrigins)
+);

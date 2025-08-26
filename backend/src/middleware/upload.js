05d2a615da0e1eeecf2e1a0288810e70119c3965
Rasya Rahmat Syaban <rasyarahmatsyban10@gmail.com>
2025-08-26 11:27:22 +0800

    refactor(upload): move upload path initialization and directory creation to the top

--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -1,16 +1,23 @@
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 // Untuk mendapatkan __dirname di ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const uploadPath = path.resolve(__dirname, "../../uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+  console.log("Folder 'uploads' belum ada, sudah dibuat:", uploadPath);
+}
+
 // Konfigurasi penyimpanan file
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     // Menggunakan path.resolve untuk path yang dinamis
-    const uploadPath = path.resolve(__dirname, "../../uploads");
     cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
